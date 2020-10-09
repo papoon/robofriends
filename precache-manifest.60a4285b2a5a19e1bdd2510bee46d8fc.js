4b62fa16bac6fcb7ef4c9a67e97c472f34c30358
Fábio Martins <fabio.martins@zap.co.ao>
2020-10-09 17:00:00 +0100

    Updates

---
+++ precache-manifest.60a4285b2a5a19e1bdd2510bee46d8fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28f4e7781b909e1eda5df584c200976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7fa0d0516761d467524d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa0d0516761d467524d",
+    "url": "/static/js/main.1a00a8b9.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
